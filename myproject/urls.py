ea2fb2240f009234ee315e9f3249e5a15a4d41b7
Vahgar8work <raghav8work@outlook.com>
2024-05-04 01:10:22 +0530

    please test login

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,5 +20,4 @@ from django.urls import path , include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('' , include("app1.urls")),
-    path('WordCount' , include("app1.urls")),
 ]
